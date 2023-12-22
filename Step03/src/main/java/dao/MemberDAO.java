f90774fb9b2ab28d36cbcc88b465e4f77b3bbf05
nam2626 <nam2626@gmail.com>
2023-12-22 17:18:43 +0900

    Servlet

--- Step03/src/main/java/dao/MemberDAO.java
+++ Step03/src/main/java/dao/MemberDAO.java
@@ -1,7 +1,11 @@
 package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
+import config.DBManager;
 import dto.MemberDTO;
 
 public class MemberDAO {
@@ -16,7 +20,34 @@ public class MemberDAO {
 	}
 
 	public ArrayList<MemberDTO> searchMember(String kind, String search) {
-		return null;
+		ArrayList<MemberDTO> list = new ArrayList<MemberDTO>();
+		
+		String sql = "select * from member where " + kind + " like '%' || ? || '%'";
+		
+		try {
+			PreparedStatement pstmt = 
+					DBManager.getInstance().getConn().prepareStatement(sql);
+			
+			pstmt.setString(1, search);
+			
+			ResultSet rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				String id = rs.getString(1);
+				String passwd = rs.getString(2);
+				String name = rs.getString(3);
+				int age = rs.getInt(4);
+				String gender = rs.getString(5);
+				list.add(new MemberDTO(id, passwd, name, age, gender));
+			}
+			
+			DBManager.getInstance().close(rs, pstmt);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return list;
 	}
 
 	
