26eee8b6de0b8c2983de2f492ee3c9b62d0ae81c
nam2626 <nam2626@gmail.com>
2023-12-26 10:50:44 +0900

    el, jstl

---
+++ JSTL/src/main/java/dto/MemberDTO.java
@@ -0,0 +1,87 @@
+package dto;
+
+public class MemberDTO {
+	private String id;
+	private String pass;
+	private String name;
+	private int age;
+	private String gender;
+	private int point;
+	private String grade;
+
+	public MemberDTO() {
+	}
+
+	public MemberDTO(String id, String pass, String name, int age, String gender, int point, String grade) {
+		this.id = id;
+		this.pass = pass;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.point = point;
+		this.grade = grade;
+	}
+
+	@Override
+	public String toString() {
+		return "MemberDTO [id=" + id + ", pass=" + pass + ", name=" + name + ", age=" + age + ", gender=" + gender
+				+ ", point=" + point + ", grade=" + grade + "]";
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	public String getGrade() {
+		return grade;
+	}
+
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+
+}
