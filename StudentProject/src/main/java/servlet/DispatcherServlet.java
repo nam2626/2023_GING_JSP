f2951e73ce73d5a3abb4bada3f120509424a88dd
nam2626 <nam2626@gmail.com>
2023-12-27 10:14:14 +0900

    Student Project

---
+++ StudentProject/src/main/java/servlet/DispatcherServlet.java
@@ -0,0 +1,48 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DispatcherServlet
+ */
+@WebServlet("/*.do")
+public class DispatcherServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DispatcherServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String[] arr = request.getRequestURI().split("/");
+		String command = arr[arr.length-1];
+		
+		//HandlerMapping에 command를 보내서 작업할 컨트롤러를 받아와야함
+		
+		//Controller 실행, 실행 결과는 어느 페이지로 이동할 것인지? 받아옴
+		
+		//페이지 이동 - 경로, forward? redirect?
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
