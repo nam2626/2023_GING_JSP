c4ff4959bd9caabc6092f24384ce0167b8db973d
nam2626 <nam2626@gmail.com>
2023-12-27 14:31:46 +0900

    Student Project

--- StudentProject/src/main/java/controller/MainController.java
+++ StudentProject/src/main/java/controller/MainController.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import dto.MajorDTO;
 import dto.StudentDTO;
 import service.StudentService;
 import view.ModelAndView;
@@ -19,8 +21,16 @@ public class MainController implements Controller {
 				StudentService.getInstance().selectAllStudent();
 		request.setAttribute("studentList", studentList);
 		//모든 학과 정보를 읽어와서 session 영역에 저장
+		ArrayList<MajorDTO> majorList = StudentService.getInstance().selectAllMajor();
+		HttpSession session = request.getSession();
+		session.setAttribute("majorList", majorList);
 		
 		return view;
 	}
 
 }
+
+
+
+
+
